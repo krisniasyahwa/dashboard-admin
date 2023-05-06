37b059a1d7af23f9c62a425074f0c1fa95908097
Andri Junaedi <andri@sawala.tech>
2023-05-06 16:52:49 +0700

    feat: add featured_image on products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,6 +19,21 @@ class Product extends Model
         return $this->hasMany(ProductGallery::class, 'products_id', 'id');
     }
 
+    public function featured_image()
+    {
+        try {
+            $featured = $this->hasOne(ProductGallery::class, 'products_id', 'id')->where('is_featured', 1);
+
+            // Error handling, if there is no featured image
+            if (!$featured)
+                return $featured;
+            else
+                return $this->hasOne(ProductGallery::class, 'products_id', 'id');
+        } catch (\Throwable $th) {
+            return null;
+        }
+    }
+
     public function category()
     {
         return $this->belongsTo(ProductCategory::class, 'categories_id', 'id');
