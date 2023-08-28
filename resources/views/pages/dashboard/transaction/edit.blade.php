3320fe532f3df97763b2ee41320daf43d2e95df2
glenkusuma <glenkusuma07@gmail.com>
2023-08-28 17:47:16 +0700

    feat(views): enhance transaction edit, view and index pages, dynamic previous page redirection, added payment status field
    This commit improves the transaction edit and index pages by introducing dynamic previous page redirection, adding a payment status field, and enhancing the user experience.
    
    Enhancements on the edit page:
    - Added dynamic previous page redirection to the edit page to ensure proper navigation.
    - Introduced a payment status field to provide more detailed information about the payment.
    - Updated the options for edited fields based on the previous transaction validation commit for consistency and accuracy.
    - Enhanced the design of the edit fields to align better with the overall views design.
    
    Enhancements on the index page:
    - Updated the fields to display the payment method and added a status payment field, providing a comprehensive overview of each transaction.
    - Added an 'Edit' button for each transaction row, linking to the edit page with the dynamic previous page parameter.
    
    Enhancements on the views page:
    - Added a 'Status Payment' field to the transaction details view, displaying the payment status.
    - Added an 'Edit' button to the transaction details view, linking to the edit page with the dynamic previous page parameter.
    
    These enhancements enhance user experience, streamline navigation, and provide more comprehensive information about transactions and their payment status.

--- resources/views/pages/dashboard/transaction/edit.blade.php
+++ resources/views/pages/dashboard/transaction/edit.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Transaction &raquo; {{ $item->name }} &raquo; Edit
+            <a class="hover:underline" href="{{route('dashboard.transaction.index')}}">Transaction</a> &raquo; <a class="hover:underline" href="{{route('dashboard.transaction.show', $item->id)}}">#{{ $item->id }}</a> &raquo; Edit
         </h2>
     </x-slot>
-
-    <div class="py-12">
+    <div class="py-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-lg text-gray-800 leading-tight mt-4">Transaction Details</h2>
             <div>
                 @if ($errors->any())
                     <div class="mb-5" role="alert">
@@ -24,46 +24,138 @@
                         </div>
                     </div>
                 @endif
-                <form class="w-full" action="{{ route('dashboard.transaction.update', $item->id) }}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    @method('put')
-                    <div class="flex flex-wrap -mx-3 mb-6">
-                        <div class="w-full px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                                Payment
-                            </label>
-                            <select name="payment" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
-                                <option value="{{ $item->payment }}">{{ $item->payment }}</option>
-                                <option disabled>-------</option>
-                                <option value="QRIS">QRIS</option>
-                                <option value="MANUAL">MANUAL</option>
-                            </select>
-                        </div>
-                    </div>
+                <div class="p-6 bg-white border-b border-gray-200">
 
-                    <div class="flex flex-wrap -mx-3 mb-6">
-                        <div class="w-full px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                                Status
-                            </label>
-                            <select name="status" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
-                                <option value="{{ $item->status }}">{{ $item->status }}</option>
-                                <option disabled>-------</option>
-                                <option value="PENDING">PENDING</option>
-                                <option value="SUCCESS">SUCCESS</option>
-                                <option value="CANCELLED">CANCELLED</option>
-                                <option value="FAILED">FAILED</option>
-                            </select>
+                    <form class="w-full" action="{{ route('dashboard.transaction.update', ['transaction' => $item->id, 'previous_page' => $previous_page]) }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        @method('put')
+                        <table class="table-auto w-full">
+                            <tbody>
+                                <tr>
+                                    <th class="border px-6 py-2 text-right ">Name</th>
+                                    <td class="border px-6 py-2">{{ $item->user->name }}</td>
+                                </tr>
+                                <tr>
+                                    <th class="border px-6 py-2 text-right">Email</th>
+                                    <td class="border px-6 py-2">{{ $item->user->email }}</td>
+                                </tr>
+                                <tr>
+                                    <th class="border px-6 py-2 text-right">Address</th>
+                                    <td class="border px-6 py-2">{{ $item->address }}</td>
+                                </tr>
+                                <tr>
+                                    <th class="border px-6 py-2 text-right">Total Price</th>
+                                    <td class="border px-6 py-2">{{ number_format($item->total_price) }}</td>
+                                </tr>
+                                <tr>
+                                    <th class="border px-6 py-2 text-right">Shipping Price</th>
+                                    <td class="border px-6 py-2">{{ number_format($item->shipping_price) }}</td>
+                                </tr>
+                                <tr>
+                                    <th class="border px-6 py-2 text-right">
+                                        <label class="block uppercase tracking-wide text-gray-700 text-l font-bold " for="grid-last-name">
+                                            Payment
+                                        </label>
+                                    </th>
+                                    <td class="border px-6 py-2">
+                                        <select name="payment" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
+                                            <option value="{{ $item->payment }}">{{ $item->payment }}</option>
+                                            <option disabled>-------</option>
+                                            <option value="MANUAL">MANUAL</option>
+                                            <option value="QRIS">QRIS</option>
+                                            <option value="CASH">CASH</option>
+                                        </select></td>
+                                </tr>
+                                <tr>
+                                    <th class="border px-6 py-2 text-right"><label class="block uppercase tracking-wide text-gray-700 text-l font-bold" for="grid-last-name">
+                                        Status Payment
+                                    </label></th>
+                                    <td class="border px-6 py-2">
+                                    <select name="status_payment" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
+                                        <option value="{{ $item->status_payment }}">{{ $item->status_payment }}</option>
+                                        <option disabled>-------</option>
+                                        <option value="UNPAID">UNPAID</option>
+                                        <option value="REVIEW">REVIEW</option>
+                                        <option value="PAID">PAID</option>
+                                        <option value="REJECTED">REJECTED</option>
+                                        <option value="EXPIRED">EXPIRED</option>
+                                    </select></td>
+                                </tr>
+                                <tr>
+                                    <th class="border px-6 py-2 text-right">
+                                        <label class="block uppercase tracking-wide text-gray-700 text-l font-bold" for="grid-last-name">
+                                            Status
+                                        </label>
+                                    </th>
+                                    <td class="border px-6 py-2">
+                                        <select name="status" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
+                                            <option value="{{ $item->status }}">{{ $item->status }}</option>
+                                            <option disabled>-------</option>
+                                            <option value="PENDING">PENDING</option>
+                                            <option value="PROCESS">PROCESS</option>
+                                            <option value="READY">READY</option>
+                                            <option value="SUCCESS">SUCCESS</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                                {{-- <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                            Payment
+                                        </label>
+                                        <select name="payment" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
+                                            <option value="{{ $item->payment }}">{{ $item->payment }}</option>
+                                            <option disabled>-------</option>
+                                            <option value="MANUAL">MANUAL</option>
+                                            <option value="QRIS">QRIS</option>
+                                            <option value="CASH">CASH</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                            Status Payment
+                                        </label>
+                                        <select name="status_payment" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
+                                            <option value="{{ $item->status_payment }}">{{ $item->status_payment }}</option>
+                                            <option disabled>-------</option>
+                                            <option value="UNPAID">UNPAID</option>
+                                            <option value="REVIEW">REVIEW</option>
+                                            <option value="PAID">PAID</option>
+                                            <option value="REJECTED">REJECTED</option>
+                                            <option value="EXPIRED">EXPIRED</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                            Status
+                                        </label>
+                                        <select name="status" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
+                                            <option value="{{ $item->status }}">{{ $item->status }}</option>
+                                            <option disabled>-------</option>
+                                            <option value="PENDING">PENDING</option>
+                                            <option value="PROCESS">PROCESS</option>
+                                            <option value="READY">READY</option>
+                                            <option value="SUCCESS">SUCCESS</option>
+                                        </select>
+                                    </div>
+                                </div> --}}
+
+
+                            </tbody>
+                        </table>
+                        <div class="flex flex-wrap -mx-3 mt-8 mb-6">
+                            <div class="w-full px-3 text-right">
+                                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                                    Update Transaction
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="flex flex-wrap -mx-3 mb-6">
-                        <div class="w-full px-3 text-right">
-                            <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                                Update Transaction
-                            </button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
