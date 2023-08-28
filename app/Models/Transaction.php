de5eec8731e11c96108626b3f9670594f2d19ebd
glenkusuma <glenkusuma07@gmail.com>
2023-08-28 17:17:28 +0700

    refactor(models): add status_payment field to Transaction model's fillable
    This commit updates the 'Transaction' model (app/Models/Transaction.php) by adding the 'status_payment' field to the 'fillable' array.
    
    The 'status_payment' field has been added to the list of mass-assignable attributes, allowing this field to be set during mass assignment, such as when creating or updating a transaction record.
    
    This change is in alignment with the recent addition of the 'status_payment' column to the 'transactions' table in the database, and it ensures that the field can be conveniently assigned when working with transaction data.

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -11,7 +11,14 @@ class Transaction extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'users_id', 'address', 'payment', 'total_price', 'shipping_price', 'status', 'point_usage'
+        'users_id',
+        'address',
+        'payment',
+        'total_price',
+        'shipping_price',
+        'status',
+        'point_usage',
+        'status_payment'
     ];
     //Create relationship with Users table->One to Many->users_id as foreign key
     public function user()
