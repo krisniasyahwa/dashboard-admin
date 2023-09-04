5bc899d0608d3875d26725bda7976f74664ca00f
Mzhssn <mzhssn313@gmail.com>
2023-09-04 08:55:26 +0700

    Fix:conflict-route-checkout

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -7,10 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use App\Models\TransactionItem;
 use App\Models\User;
-<<<<<<< HEAD
-use App\Models\Group;
-=======
->>>>>>> main
 use App\Models\Product;
 use App\Traits\FilterByDate;
 use Exception;
@@ -104,38 +100,6 @@ class TransactionController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-
-
-    public function validatecart($items)
-    {
-        //Get the first merchant_id from first item
-
-        $merchant_id = $items[0]['product']['merchants_id'];
-        // //Loop to check if all item have same merchant_id
-        foreach ($items as $item) {
-
-            if ($item['product']['merchants_id'] !== $merchant_id) {
-                return false; //If item have different merchant_id return false
-            }
-        }
-
-        return true; //If all item have same merchant_id return true
-    }
-
-    public function validationusergroups($user)
-    {
-        $groupRegistered = Group::pluck('group_name');
-        $user = User::with('usergroup.group')->where('id', $user)->get();
-        $userGroup = $user[0]['usergroup']['group']['group_name'];
-        foreach ($groupRegistered as $group) {
-            if ($group === $userGroup) {
-                return $userGroup;
-            }
-        }
-
-
-        return false;
-    }
 
     public function potentialpoint($user, $items, $validatedDataTransaction)
     {
@@ -308,78 +272,74 @@ class TransactionController extends Controller
         return $product;
     }
 
-    public function checkout(Request $request)
-    {
-        $user = Auth::user()->id;
+    // public function checkout(Request $request)
+    // {
+    //     $user = Auth::user()->id;
 
-        $validatedDataTransaction = $request->validate([
-            'address' => "nullable",
-            'total_price' => "required",
-            'status' => "required|in:PENDING,SUCCESS,CANCELLED,FAILED,ONPROSSES",
-            'payment' => 'required|in:QRIS,MANUAL',
-            'point_usage' => "required|min:0",
-            'items' => "required|array",
-            'items.*.id' => "required|exists:products,id",
-            'items.*.quantity' => "required|min:1",
-            'items.*.note' => "nullable",
-        ]);
+    //     $validatedDataTransaction = $request->validate([
+    //         'address' => "nullable",
+    //         'total_price' => "required",
+    //         'status' => "required|in:PENDING,SUCCESS,CANCELLED,FAILED,ONPROSSES",
+    //         'payment' => 'required|in:QRIS,MANUAL',
+    //         'point_usage' => "required|min:0",
+    //         'items' => "required|array",
+    //         'items.*.id' => "required|exists:products,id",
+    //         'items.*.quantity' => "required|min:1",
+    //         'items.*.note' => "nullable",
+    //     ]);
 
-        $items = $validatedDataTransaction['items'];
+    //     $items = $validatedDataTransaction['items'];
 
-        if (!$this->validatecart($items)) {
-            return response()->json([
-                'message' => 'Checkout failed',
-                'data' => 'Item from different merchant'
-            ]);
-        } else {
-            $UserGroup = $this->validationusergroups($user);
-            if ($UserGroup) {
-                $promoproduct = $this->validationpromoprice($items);
-                return response()->json([
-                    'message' => 'Checkout successful',
-                    'data' => $promoproduct,
-                ]);
-            }elseif(!$UserGroup){
-                return response()->json([
-                    'message' => 'Checkout successful',
-                    'data' => $UserGroup,
-                ]);
-            }
+    //     if (!$this->validatecart($items)) {
+    //         return response()->json([
+    //             'message' => 'Checkout failed',
+    //             'data' => 'Item from different merchant'
+    //         ]);
+    //     } else {
+    //         $UserGroup = $this->validationusergroups($user);
+    //         if ($UserGroup) {
+    //             $promoproduct = $this->validationpromoprice($items);
+    //             return response()->json([
+    //                 'message' => 'Checkout successful',
+    //                 'data' => $promoproduct,
+    //             ]);
+    //         }elseif(!$UserGroup){
+    //             return response()->json([
+    //                 'message' => 'Checkout successful',
+    //                 'data' => $UserGroup,
+    //             ]);
+    //         }
 
 
 
 
             
-        }
+    //     }
 
 
+    //     $transaction = Transaction::create([
+    //         'users_id' => $user,
+    //         'address' => $validatedDataTransaction['address'],
+    //         'total_price' => $validatedDataTransaction['total_price'],
+    //         'status' => $validatedDataTransaction['status'],
+    //         'payment' => $validatedDataTransaction['payment'],
+    //         'point_usage' => $validatedDataTransaction['point_usage'],
 
+    //     ]);
 
+    //     foreach ($items as $item){
+    //         TransactionItem::create([
+    //             'users_id' => $user,
+    //             'products_id' => $item['id'],
+    //             'transactions_id' => $transaction['id'],
+    //             'quantity' => $item['quantity'],
+    //             'note' =>  $item['note'],
+    //         ]);
+    //     }
 
+    //     return ResponseFormatter::success($transaction->load('items.product'), 'Transaksi berhasil');
 
-        // $transaction = Transaction::create([
-        //     'users_id' => $user,
-        //     'address' => $validatedDataTransaction['address'],
-        //     'total_price' => $validatedDataTransaction['total_price'],
-        //     'status' => $validatedDataTransaction['status'],
-        //     'payment' => $validatedDataTransaction['payment'],
-        //     'point_usage' => $validatedDataTransaction['point_usage'],
-
-        // ]);
-
-        // foreach ($items as $item){
-        //     TransactionItem::create([
-        //         'users_id' => $user,
-        //         'products_id' => $item['id'],
-        //         'transactions_id' => $transaction['id'],
-        //         'quantity' => $item['quantity'],
-        //         'note' =>  $item['note'],
-        //     ]);
-        // }
-
-        //return ResponseFormatter::success($transaction->load('items.product'), 'Transaksi berhasil');
-
-    }
+    // }
 
     public function confirmation(Request $request)
     {
@@ -566,23 +526,7 @@ class TransactionController extends Controller
         }
     }
 
-    // public function payments($totalprice, Request $request )
-    // {
-    //     $user = Auth::user()->id;
-    //     $method = $request->input('method');
-    //     $result =  Transaction::where('users_id', $user)->orderBy('created_at', 'desc')->first();
-    //     if ($result) {
-    //         //Update Total Price
-    //         $result->total_price = $totalprice;
-    //         //Updated Payments
-    //         $result->payment = $method;
-    //         //Save Updated
-    //         $result->save();
-    //         return ResponseFormatter::success($result, 'success');
-    //     }else{
-    //         return ResponseFormatter::error(null, 'error');
-    //     }
-    // }
+
 
     public function payments(Request $request){
         $user = Auth::user()->id;
