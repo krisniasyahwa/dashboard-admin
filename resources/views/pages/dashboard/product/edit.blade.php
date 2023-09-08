0cd25a54e13bcd325db77188abed5074320aef48
glenkusuma <glenkusuma07@gmail.com>
2023-09-08 17:31:19 +0700

    feat(views): Add input fields for stock, promo price, takeway charge, and best seller; handle old values and defaults in create and edit views
    These commits update create and edit views for products. They introduce input fields for stock, promo price, takeway charge, and best seller, along with logic to handle old values and provide appropriate default values.

--- resources/views/pages/dashboard/product/edit.blade.php
+++ resources/views/pages/dashboard/product/edit.blade.php
@@ -79,7 +79,16 @@
                             <textarea name="description" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Product Description">{{ old('description') ?? $item->description }}</textarea>
                         </div>
                     </div>
-                    
+
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                Stock
+                            </label>
+                            <input value="{{ old('stock') ?? $item->stock }}" name="stock" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Product Stock">
+                        </div>
+                    </div>
+
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
@@ -88,29 +97,49 @@
                             <input value="{{ old('price') ?? $item->price }}" name="price" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Product Price">
                         </div>
                     </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                promo_price
+                            </label>
+                            <input value="{{ old('promo_price') ?? $item->promo_price }}" name="promo_price" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Product promo_price">
+                        </div>
+                    </div>
 
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                                FAVORITE
+                                takeway_charge
+                            </label>
+                            <input value="{{ old('takeway_charge') ?? $item->takeway_charge }}" name="takeway_charge" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Product takeway_charge">
+                        </div>
+                    </div>
+
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                Best Seller
                             </label>
 
                             <div class="mt-2">
                                 <label class="inline-flex items-center">
-                                    <input type="radio" class="form-radio" name="favorite" value="1" {{ $item->favorite == 1 ? 'checked' : '' }}>
+                                    <input type="radio" class="form-radio" name="best_seller" value="1" {{ $item->best_seller == 1 ? 'checked' : '' }}>
                                     <span class="ml-2">Yes</span>
                                 </label>
                                 <label class="inline-flex items-center ml-6">
-                                    <input type="radio" class="form-radio" name="favorite" value="0" {{ $item->favorite == 0 ? 'checked' : '' }}>
+                                    <input type="radio" class="form-radio" name="best_seller" value="0" {{ $item->best_seller == 0 ? 'checked' : '' }}>
                                     <span class="ml-2">No</span>
                                 </label>
                             </div>
                         </div>
                     </div>
-
+                    <input type="hidden" name="previous_page" value="{{$previous_page}}">
                     <div class="flex flex-wrap -mx-3 mb-6">
-                        <div class="w-full px-3 text-right">
-                            <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                        <div class="px-3 text-right ml-auto space-x-8">
+                            <a href="{{$previous_page}}" class="border border-red-500 bg-red-500 text-white font-bold py-2 px-4 rounded-md transition duration-500 ease select-none hover:bg-red-600 mr-auto">
+                                Cancel Edit
+                            </a>
+                            <button type="submit" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-md transition duration-500 ease select-none focus:outline-none focus:shadow-outline ml-auto">
                                 Update Product
                             </button>
                         </div>
@@ -119,4 +148,5 @@
             </div>
         </div>
     </div>
+    {{-- <pre>@php var_dump($item); @endphp</pre> --}}
 </x-app-layout>
