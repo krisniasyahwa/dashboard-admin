99e59280f8034f1cd405c747ec3781b9cc78e36e
glenkusuma <glenkusuma07@gmail.com>
2023-09-22 21:19:34 +0700

    feat(view): enhance transaction details and add QRIS transaction proof
    Improved the 'show.blade.php' view for displaying transaction details in the dashboard: Added a 'Transaction Type' column to display the transaction type. Added a 'Payment Type' column to display the payment type. Added a 'Payment Method' column to display the payment method. Renamed 'Status Payment' to 'Status Order' for clarity. Added an image section to display the QRIS transaction proof. These changes enhance the representation of transaction details and provide a visual representation of the QRIS transaction proof.

--- resources/views/pages/dashboard/transaction/show.blade.php
+++ resources/views/pages/dashboard/transaction/show.blade.php
@@ -14,9 +14,11 @@
                 },
                 columns: [
                     { data: 'id', name: 'id', width: '5%'},
+                    { data: 'products_id', name: 'products_id', width: '5%'},
                     { data: 'product.name', name: 'product.name' },
                     { data: 'product.price', name: 'product.price' },
                     { data: 'quantity', name: 'quantity' },
+                    { data: 'note', name: 'note', width: '50%'},
                 ],
             });
         </script>
@@ -26,8 +28,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <h2 class="font-semibold text-lg text-gray-800 leading-tight mb-4">Transaction Details</h2>
 
-            <div class="bg-white overflow-hidden shadow sm:rounded-lg mb-4">
-                 <div class="p-6 bg-white border-b border-gray-200">
+            <div class="flex bg-white overflow-hidden shadow sm:rounded-lg mb-4">
+                 <div class="p-6 bg-white border-b border-gray-200 w-1/2">
                     <table class="table-auto w-full">
                         <tbody>
                             <tr>
@@ -51,28 +53,40 @@
                                 <td class="border px-6 py-2">{{ number_format($transaction->shipping_price) }}</td>
                             </tr>
                             <tr>
-                                <th class="border px-6 py-2 text-right">Payment</th>
+                                <th class="border px-6 py-2 text-right">Transaction Type</th>
+                                <td class="border px-6 py-2">{{ $transaction->transaction_type }}</td>
+                            </tr>
+                            <tr>
+                                <th class="border px-6 py-2 text-right">Payment Method</th>
                                 <td class="border px-6 py-2">{{ $transaction->payment }}</td>
+                            </tr>
+                            <tr>
+                                <th class="border px-6 py-2 text-right">Payment Type </th>
+                                <td class="border px-6 py-2">{{ $transaction->payment_type }}</td>
                             </tr>
                             <tr>
                                 <th class="border px-6 py-2 text-right">Status Payment</th>
                                 <td class="border px-6 py-2">{{ $transaction->status_payment }}</td>
                             </tr>
                             <tr>
-                                <th class="border px-6 py-2 text-right">Status</th>
+                                <th class="border px-6 py-2 text-right">Status Order</th>
                                 <td class="border px-6 py-2">{{ $transaction->status }}</td>
                             </tr>
                             <tr>
                                 <th class="border px-6 py-2 text-right">Action</th>
                                 <td class="border px-6 py-2">
                                     <a class="inline-block border border-gray-700 bg-gray-700 text-white rounded-md px-8 py-1 m-1 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
-                            href="{{route('dashboard.transaction.edit', ['transaction' => $transaction->id, 'previous_page' => $previous_page ?? 'show'])}}">
-                            Edit
-                        </a>
+                                        href="{{route('dashboard.transaction.edit', ['transaction' => $transaction->id, 'previous_page' => $previous_page ?? 'show'])}}" onclick="storeScrollPosition()">
+                                        Edit
+                                    </a>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200 w-1/2">
+                    <h3 class="font-semibold text-lg text-gray-800 leading-tight mb-4"> Bukti Transaksi QRIS</h3>
+                    <img class="max-h-96" src="{{ $transaction->payment_image ?? 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Empty_set_symbol.svg/640px-Empty_set_symbol.svg.png'}}" alt="">
                 </div>
             </div>
 
@@ -83,9 +97,11 @@
                         <thead>
                         <tr>
                             <th>ID</th>
+                            <th>ID_P</th>
                             <th>Produk</th>
                             <th>Harga</th>
                             <th>Qty</th>
+                            <th>Notes</th>
                         </tr>
                         </thead>
                         <tbody></tbody>
@@ -94,4 +110,11 @@
             </div>
         </div>
     </div>
+    <script>
+        // Run the setScrollPosition function when the page loads
+        $(document).ready(function() {
+            setScrollPosition();
+            clearScrollPosition()
+        });
+    </script>
 </x-app-layout>
