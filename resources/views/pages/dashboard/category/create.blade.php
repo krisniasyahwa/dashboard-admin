3315c150e99444806b69cc43f741c7d94de42f6d
glenkusuma <glenkusuma07@gmail.com>
2023-09-25 18:01:56 +0700

    feat(views): Add image_path field to category create, edit, and index views
    In the category create, edit, and index views, added the 'image_path' field for displaying and uploading images. This enhancement improves the user interface for managing category images.

--- resources/views/pages/dashboard/category/create.blade.php
+++ resources/views/pages/dashboard/category/create.blade.php
@@ -48,6 +48,15 @@
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                Category Image
+                            </label>
+                            <img id="image-path-preview" src="{{old('image_path') ?? 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Empty_set_symbol.svg/640px-Empty_set_symbol.svg.png'}}" class="rounded-full w-40 h-40 mb-4"/>
+                            <input id="image-path-input" accept="image/*" value="{{ old('image_path') }}" name="image_path" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="file" placeholder="Merchant Photo Profile">
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3 text-right">
                             <button type="submit" class=" shadow-lg bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                                 Save Category
@@ -58,4 +67,10 @@
             </div>
         </div>
     </div>-
+    <x-slot name="script">
+        <script>
+             // Use the function for image_path uploads
+            handleImageUpload('image-path-input', 'image-path-preview',);
+        </script>
+    </x-slot>
 </x-app-layout>
